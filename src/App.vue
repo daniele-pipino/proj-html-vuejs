66d8eff28a8c3d6178ee661f4b57a2f8b0c9be53
daniele-pipino <Danielemacpipino@icloud.com>
2021-09-15 13:00:59 +0200

    Navbar completa

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <div id="app">My Project</div>
+  <div id="app">
+    <Header />
+    <Main />
+    <Footer />
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
+import Main from "@/components/Main.vue";
+import Footer from "@/components/Footer.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
 };
 </script>
 
 <style lang="scss">
-#app {
-}
+@import "scss/style.scss";
 </style>
