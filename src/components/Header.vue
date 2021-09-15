65591dff122793f26851231d557c47355a917e66
daniele-pipino <Danielemacpipino@icloud.com>
2021-09-15 14:47:25 +0200

    Jumbo aggiunto - Header completo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,75 +1,21 @@
 <template>
-  <header class="d-flex justify-content-between mx-4">
-    <!-- logo -->
-    <div class="logo pt-3">
-      <img src="@/assets/img/logo-sidearea-1.png" alt="" class="img-fluid" />
-    </div>
-    <!-- navbar -->
-    <div class="nav-bar d-flex">
-      <ul class="d-flex align-items-center justify-content-center pt-2">
-        <li v-for="(item, index) in navbar" :key="index" class="mx-3 fw-bold">
-          <a href="#">{{ item.element }}</a>
-        </li>
-        <li class="mx-3 fw-bold">
-          <a href="#"
-            ><img
-              src="@/assets/svg/svg-1.svg"
-              alt="dot.menu"
-              class="dot-menu pb-1"
-          /></a>
-        </li>
-      </ul>
-    </div>
-  </header>
+  <div>
+    <Navbar />
+    <Jumbo />
+  </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
+import Jumbo from "@/components/Jumbo.vue";
 export default {
   name: "Header",
-  data() {
-    return {
-      navbar: [
-        {
-          element: "HOME",
-        },
-        {
-          element: "PAGES",
-        },
-        {
-          element: "PORTFOLIO",
-        },
-        {
-          element: "BLOG",
-        },
-        {
-          element: "SHOP",
-        },
-        {
-          element: "ELEMENTS",
-        },
-      ],
-    };
+  components: {
+    Navbar,
+    Jumbo,
   },
 };
 </script>
 
 <style lang="scss" scoped>
-header {
-  height: 70px;
-  .nav-bar {
-    ul li {
-      list-style-type: none;
-    }
-    a {
-      text-transform: uppercase;
-      text-decoration: none;
-      color: black;
-      font-size: 13px;
-      line-height: 70px;
-    }
-    .dot-menu {
-      width: 20px;
-    }
-  }
-}
 </style>
---
+++ src/components/Jumbo.vue
@@ -0,0 +1,98 @@
+<template>
+  <!-- start jumbo -->
+  <div class="jumbo mx-3 mt-5">
+    <div class="container">
+      <div class="row">
+        <!-- left -->
+
+        <div class="col-6 d-flex justify-content-end">
+          <div class="col-10">
+            <!-- title -->
+            <h1 class="jumbo-title fw-light text-right mb-4">
+              Devotion that never ends
+            </h1>
+            <!-- subtitle -->
+            <p class="jumbo-subtitle mb-5">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni
+              qui corrupti enim error a eaque libero quisquam, natus vero
+              deserunt explicabo numquam ab ad totam tenetur culpa minima
+              consequuntur ipsa.
+            </p>
+            <!-- button -->
+            <a href="#" class="btn jumbo-btn">READ MORE</a>
+          </div>
+        </div>
+
+        <!-- right -->
+
+        <div class="col-6 d-flex">
+          <!-- left -->
+          <div class="d-flex flex-column h-100 justify-content-between">
+            <img
+              src="@/assets/img/short-slider-rev-1-img-6.png"
+              alt=""
+              class="w-50 ms-5"
+            />
+            <img
+              src="@/assets/img/short-slider-rev-1-img-10.png"
+              alt=""
+              class="w-50"
+            />
+            <img
+              src="@/assets/img/short-slider-rev-1-img-8.png"
+              alt=""
+              class="w-50 ms-4"
+            />
+          </div>
+          <!-- center -->
+          <div class="d-flex">
+            <img src="@/assets/img/jumbogirl.png" alt="" class="w-50" />
+            <img src="@/assets/img/jumbo-boy.png" alt="" class="w-50 ms-1" />
+          </div>
+          <!-- right -->
+          <div class="d-flex flex-column h-100 justify-content-between">
+            <img
+              src="@/assets/img/slider-img-14.png"
+              alt=""
+              class="w-50 me-5"
+            />
+            <img
+              src="@/assets/img/short-slider-rev-1-img-6.png"
+              alt=""
+              class="w-50"
+            />
+            <img
+              src="@/assets/img/slider-img-17.png"
+              alt=""
+              class="w-50 me-4"
+            />
+          </div>
+        </div>
+        <!-- end col -->
+      </div>
+    </div>
+  </div>
+  <!-- end jumbo -->
+</template>
+
+<script>
+export default {
+  name: "Jumbo",
+};
+</script>
+
+<style scoped lang="scss">
+.jumbo {
+  .jumbo-title {
+    font-size: 75px;
+  }
+  .jumbo-subtitle {
+    color: #878787;
+  }
+  .jumbo-btn {
+    border: 2px solid #f5d8ca;
+    border-radius: 0;
+    padding: 7px 18px;
+  }
+}
+</style>
